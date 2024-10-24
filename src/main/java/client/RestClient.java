b6d738e806e10de7fd4817e44a05b90198f1ddce
Saumya Singh <singhsaumyait034@gmail.com>
2024-10-24 15:01:42 +0530

    Rest Client Class Updates with Authentication Mechanism

--- src/main/java/client/RestClient.java
+++ src/main/java/client/RestClient.java
@@ -1,12 +1,13 @@
 package client;
 
+import configReader.ConfigManager;
 import constants.AuthType;
 import custom_exception.FrameworkException;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
-import static io.restassured.RestAssured.given;
+import java.util.Base64;
 
 public class RestClient {
 
@@ -16,15 +17,22 @@ public class RestClient {
 
         switch(authType){
             case BEARER_TOKEN:
-                requestSpecification.header("Authorization", "Bearer ");
+                requestSpecification.header("Authorization", "Bearer "+ ConfigManager.get("bearerToken"));
                 break;
             case NO_AUTH:
+                System.out.println("Auth is not required .....");
                 break;
             case OAUTH2:
+                requestSpecification.header("Authorization", generateOAuth2Token());
+                break;
+            case BASIC_AUTH:
+                requestSpecification.header("Authorization", generateBasicAuthToken());
                 break;
             case API_KEY:
+                requestSpecification.header("x-api-key", ConfigManager.get("apiKey"));
                 break;
             case CONTACTS_BEARER_TOKEN:
+                requestSpecification.header("Authorization", "Bearer "+ConfigManager.get("contacts_bearer_Token"));
                 break;
             default:
                 System.out.println("The Auth is not supported. Please pass the right AUTH Type");
@@ -33,5 +41,22 @@ public class RestClient {
         }
         return requestSpecification;
 
+    }
+
+    private String generateBasicAuthToken() {
+        String credentials = ConfigManager.get("basicUsername")+ ":" +ConfigManager.get("basicPassword");
+        return Base64.getEncoder().encodeToString(credentials.getBytes());
+
+    }
+
+    private String generateOAuth2Token() {
+        return RestAssured.given()
+                .formParam("client_id", ConfigManager.get("clientId"))
+                .formParam("client_password", ConfigManager.get("clientSecret"))
+                .formParam("grant_type", ConfigManager.get("grantType"))
+                .post(ConfigManager.get("tokenUrl"))
+                .then()
+                .extract()
+                .path("access_token");
     }
 }
---
+++ src/main/java/configReader/ConfigManager.java
@@ -0,0 +1,28 @@
+package configReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+
+    private static Properties properties = new Properties();
+
+    static {
+        try(InputStream input = ConfigManager.class.getClassLoader().getResourceAsStream("config/config.properties")){
+            if(input !=null) {
+                properties.load(input);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String get(String key){
+        return properties.getProperty(key);
+    }
+
+    public static void setProperties(String key, String value){
+        properties.setProperty(key, value);
+    }
+}
